2ac2a99052ae8c8e0919a5c625a2fd39ba19887c
enels <enomauwaifo@gmail.com>
2022-12-09 06:25:40 +0100

    print string using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -  helps prints a string with the printf function
+ *
+ * Return:  returns a 0 if no error else -1
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
