0ad57a8d72b5826e9fcf9e7c31259a9b0fec071f
enels <enomauwaifo@gmail.com>
2022-12-09 08:11:51 +0100

    print to stderr using fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a string to standard error
+ *
+ * Return: returns 1 when it all runs well
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
