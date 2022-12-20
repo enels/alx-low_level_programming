fb751901ba81e7be2a6e110257cdfc589f4e4196
enels <enomauwaifo@gmail.com>
2022-12-20 14:01:35 +0100

    change value pointed to by variable to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value a pointer points to
+ * @n: variable that points to the value in memory
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
