a576e634cb7e5a8a4889332a5fab208c5acf6bab
enels <enomauwaifo@gmail.com>
2022-12-20 20:26:23 +0100

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - print the reverse of a string
+ * @s: string argumeent to print the reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int mid_value, counter, length_of_s = 0;
+	char c, temp;
+	char *p = s;
+
+	/* get the length of the string */
+	c = *s;
+	while (c != '\0')
+	{
+		length_of_s++;
+		s++;
+		c = *s;
+	}
+
+	mid_value = length_of_s / 2;
+	counter = 0;
+	s--;
+	while (counter <= mid_value)
+	{
+		temp = *s;
+		*s = p[counter];
+		p[counter] = temp;
+		counter++;
+		s--;
+	}
+}
