f8a37f4aca540d32ee53f6a883fa24ad84e64010
enels <enomauwaifo@gmail.com>
2022-12-21 05:50:21 +0100

    add spaces around '-'

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,16 +13,11 @@ void print_array(int *a, int n)
 {
 	int index;
 
-	if (n == 0 || n == -1)
-		printf("%i\n", a[0]);
-	else
+	for (index = 0; index <= n - 1; ++index)
 	{
-		for (index = 0; index <= n-1; ++index)
-		{
-			if (index != n - 1)
-				printf("%d, ", a[index]);
-			else
-				printf("%d\n", a[index]);
-		}
+		if (index != n - 1)
+			printf("%d, ", a[index]);
+		else
+			printf("%d\n", a[index]);
 	}
 }
