111170571779d537dd4bc0cc2cda50be9d492ff4
enels <enomauwaifo@gmail.com>
2022-12-21 11:32:34 +0100

    create function to convert string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include<stdio.h>
+#include <stdbool.h>
+
+/**
+ * _atoi - converts string to integers
+ * @s: string to convert
+ *
+ * Return: integer converted
+ */
+
+int _atoi(char *s)
+{
+	int index = 0, number = 0, initial_char_int_index = 0;
+	bool end = false, is_char_int = false;
+
+	/* skips none character integers */
+	while (!(s[index] >= '0' && s[index] <= '9'))
+	{
+		index++;
+		continue;
+	}
+
+	/* stores the first index where it encounters a character integer index */
+	initial_char_int_index = index;
+
+	/* loops through the string to check for */
+	while (s[index] != '\0' && end != true)
+	{
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			/* convert the character integer to integer */
+			number += s[index] - '0';
+
+			/* multiply the converted integer by 10 */
+			number *= 10;
+
+			/* notify program it has encountered a character integer */
+			is_char_int = true;
+		}
+		else if (!(s[index] >= '0' && s[index] <= '9') && is_char_int == true)
+			end = true;
+
+		++index;
+	}
+
+	/* remove the trailing zero */
+	number /= 10;
+
+	if (s[initial_char_int_index - 1] == '-' && s[index] == '\0')
+		number *= -1;
+
+	return (number);
+}
