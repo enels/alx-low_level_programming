7f3a30689d658d1d6e50367103fc4b18f2ebaa20
enels <enomauwaifo@gmail.com>
2023-01-02 11:05:15 +0100

    make it betty compliant

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - replaces memory with constant bytes
- * s: memory location of string to be replaced
- * b: character to fill the memory with
- * n: number of characters to replace in s
+ * @s: memory location of string to be replaced
+ * @b: character to fill the memory with
+ * @n: number of characters to replace in s
  *
  * Return: pointer to the memory location
  */
@@ -13,10 +13,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	char *s_ptr = s;
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
 
-	return s_ptr;
+	return (s_ptr);
 }
