cd90559d2e3e1ee4b44ae7670c2c2b22b762d164
enels <enomauwaifo@gmail.com>
2023-01-02 19:41:00 +0100

    create the print_diagsums.c for summing diagonal of a matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sum of the diagonals of a matrix
+ * @a; pointer to the matric values to be addded
+ * @size: size of the matric
+ *
+ * Return: null
+ */
+
+void print_diagsums(int *a, int size)
+{ 
+	int row = size, col = size;
+	int row_count = 0, col_count = 0;
+	/* set the previous column and previous row to the top right position */
+	int prev_row_count = 0, prev_col_count = size - 1;
+	int diagsum1 = 0, diagsum2 = 0;
+
+	while (row_count < row)
+	{
+		col_count = 0;
+		while (col_count < col)
+		{
+			if (col_count == row_count)
+			{
+				diagsum1 += *a;
+			}
+
+			if ((row_count - prev_row_count == 1) && (prev_col_count - col_count == 1))
+			{
+				diagsum2 += *a;
+				prev_row_count = row_count;
+				prev_col_count = col_count;
+			}
+			/* at the top right corner of the matrix */
+			else if (row_count == 0 && col_count == size -1)
+			{ 
+				diagsum2 += *a;
+			}
+			a++;
+			col_count++;
+		} 
+		row_count++;
+	}
+
+	printf("%d, %d\n", diagsum1, diagsum2);
+}
