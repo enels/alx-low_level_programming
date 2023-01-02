f7b304e08a34c136fbd23eeaa894a47f807767b9
enels <enomauwaifo@gmail.com>
2023-01-02 17:30:38 +0100

    create the strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdbool.h>
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search from
+ * @needle: string to search for
+ *
+ * Return: pointer to the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int pos = 0;
+	char *needle_ptr = needle;
+	bool found = false;
+
+
+	while (*haystack != '0' && !found)
+	{
+		pos++;
+		/* haystack equal to first letter of needle */
+		needle_ptr = needle;
+		if (*haystack == *needle_ptr)
+		{
+			found = true;
+			while (*needle != '\0' && found)
+			{
+				haystack++, needle++;
+			}
+			/* if it got to the end of the needle */
+			/* needle found in haystack */
+			if (*needle == '\0')
+			{
+				found = true;
+				
+			}
+		}
+
+		haystack++;
+	}
+
+	return (haystack);
+}
