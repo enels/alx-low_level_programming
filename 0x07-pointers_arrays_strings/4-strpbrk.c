8a6d8c7c6a325b9d13e47b46df6a9d892c0e694a
enels <enomauwaifo@gmail.com>
2023-01-02 16:11:11 +0100

    create the strpbrk.c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * _strpbrk - searches a string for any sets of bytes
+ * @s: string to search
+ * @accept: string to search for
+ *
+ * Return: pointer to the start of bytes in s to the end of s
+ */ 
+
+char *_strpbrk(char *s, char *accept)
+{
+	bool found = false;
+	char *accept_ptr = accept;
+
+	while (*s != '\0' && !found)
+	{
+		/* sets accept to begnning of string */
+		accept = accept_ptr;
+		while (*accept != '\0' && !found)
+		{
+			if (*accept == *s)
+			{
+				found = true;
+			}
+			accept++;
+		}
+
+		s++;
+	}
+	s--;
+	return s; 
+}
