e4907617c89f846c6a66bf5f38bd3cbff4f7d8c6
enels <enomauwaifo@gmail.com>
2023-01-04 12:17:46 +0100

    make it betty compliant

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,12 @@
 /**
  * _puts_recursion - recusively prints out line
  * @s: string to print out
- * 
+ *
  * Return: void
  */
 
 void _puts_recursion(char *s)
 {
-		
 	if (*s != '\0')
 	{
 		_putchar(*s);
