f02407df2722e3c73b154d9afae8877450110bda
enels <enomauwaifo@gmail.com>
2023-01-04 19:19:45 +0100

    create 2-strlen_recursion for finding the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - recursively print the length of a string
+ * @s: string to print its length
+ *
+ * Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+
+	return 1 + _strlen_recursion(++s);
+
+}
