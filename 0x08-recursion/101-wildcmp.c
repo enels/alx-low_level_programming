9ef5cef26d0e6ec4ad1ff992c1654ad17087e03f
enels <enomauwaifo@gmail.com>
2023-01-05 18:04:12 +0100

    make it betty-style compliant

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,7 +8,7 @@
  * Return: 1 if strings are identical, 0 otherwise
  */
 
-int wildcmp(char *s1, char* s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*' || *s1 != '\0')
 	{
@@ -18,7 +18,7 @@ int wildcmp(char *s1, char* s2)
 		}
 		else
 		{
-			s1++, s2++; 
+			s1++, s2++;
 		}
 
 		wildcmp(s1, s2);
@@ -26,7 +26,7 @@ int wildcmp(char *s1, char* s2)
 
 	if (*s1 == '\0' || *s2 == '\0' || *s2 == '*')
 	{
-		return (1); 
+		return (1);
 	}
 	else
 	{
