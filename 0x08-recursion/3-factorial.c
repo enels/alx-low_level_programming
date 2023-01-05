d90f38aea57b04bab2eabc4757ad4012b54ce3d2
enels <enomauwaifo@gmail.com>
2023-01-05 02:29:09 +0100

    make the factorial func

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - finds the factorial of a given number recursively
+ * @n: number to find its factorial
+ *
+ * Return: the factorial in int. if n is -1 then return -1 (error).
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return 1;
+	}
+	
+	if (n < 0)
+	{
+		return -1;
+	}
+
+	return (n * factorial(n - 1));
+}
