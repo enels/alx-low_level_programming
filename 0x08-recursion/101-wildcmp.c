7f8423840049bea3299d78efa5282d50a82bd094
enels <enomauwaifo@gmail.com>
2023-01-05 18:01:29 +0100

    create 101-wildcmp file to compare two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * wildcmp - compaes 2 strings
+ * @s1: first string to comp
+ * @s2: second string to comp
+ *
+ * Return: 1 if strings are identical, 0 otherwise
+ */
+
+int wildcmp(char *s1, char* s2)
+{
+	if (*s2 == '*' || *s1 != '\0')
+	{
+		if (*s2 == '\0')
+		{
+			s1++;
+		}
+		else
+		{
+			s1++, s2++; 
+		}
+
+		wildcmp(s1, s2);
+	}
+
+	if (*s1 == '\0' || *s2 == '\0' || *s2 == '*')
+	{
+		return (1); 
+	}
+	else
+	{
+		return (0);
+	}
+}
