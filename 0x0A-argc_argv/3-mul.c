b9796804336d92d57b440d3f5e36b9e779e4be8f
enels <enomauwaifo@gmail.com>
2023-01-07 16:24:15 +0100

    multiply 2 ints

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 3-mul - prints result of multiplying 2 nums
+ * @argv: pointer to array containing the nums
+ * @argc: count of the args
+ *
+ * Return: 0 if success, otherwise return 1 and print Error
+ */
+
+int main(int argc, char **argv)
+{
+	long n1;
+	long n2;
+
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+		return 1;
+	} 
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	
+	printf("%ld\n", n1 * n2);
+
+	return 0;	
+}
