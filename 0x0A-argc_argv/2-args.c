f338fa0fdf56c376e212878fc596219778cb71ec
enels <enomauwaifo@gmail.com>
2023-01-07 15:52:36 +0100

    print out all names of arguments entered

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument passed to it
+ * @argv: array pointing to the argumeents
+ * @argc: count of number of args
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	int count = 0;
+
+	if (argv[0])
+	{
+		 while (count < argc)
+		 {
+	     	printf("%s\n", argv[count]);
+		 	count++;
+		 } 
+	}
+
+	return (0);
+}
