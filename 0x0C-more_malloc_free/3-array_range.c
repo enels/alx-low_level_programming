b3ab4db151db34d000f17d0be4f29e83112c907e
enels <enomauwaifo@gmail.com>
2023-01-19 17:31:48 +0100

    create func to create array of ints using malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * array_range - create array of integers
+ * @min: array lower boundary
+ * @max: array upper boundary
+ *
+ * Return: int *
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr_ptr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	arr_ptr = malloc(sizeof(int) * (max - min));
+
+	if (arr_ptr != NULL)
+	{ 
+		return (arr_ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
