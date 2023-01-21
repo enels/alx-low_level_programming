ccca06cba46dafb84f5a63c4b0b69cd08e53c5bd
enels <enomauwaifo@gmail.com>
2023-01-21 12:55:12 +0100

    remove trailing whitespaces - betty compliant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	int num;
 	unsigned int i;
-	va_list args; 
+	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
