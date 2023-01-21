6354c7bea3c29b1fa6b22550dfabb7455e9ad808
enels <enomauwaifo@gmail.com>
2023-01-21 12:03:41 +0100

    sum all variables of a variadic func

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all the variables
+ * @n: first variable in the list
+ *
+ * Return: total sum obtained
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i; 
+	va_list ap;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+
+	return (sum);
+}
