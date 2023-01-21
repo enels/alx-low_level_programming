c41083a2bfa659bda8743ad4e35d0d49d7c481d9
enels <enomauwaifo@gmail.com>
2023-01-21 12:42:22 +0100

    print variable amount of numbers passed as argument to a func

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - variadic func that print numbers
+ * @separator: number separator
+ * @n: initial number of the series of numbers to print out
+ *
+ * Return: void
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int num;
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	num = n;
+	for (i = 0; i <= n; i++)
+	{
+		if (i == (n))
+		{
+			printf("%d", num);
+		}
+		else
+		{
+			printf("%d%s", num, separator);
+		}
+		num = va_arg(args, int);
+	}
+	va_end(args);
+
+	printf("\n");
+}
