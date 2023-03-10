7c7f9dd0211abe1602a62eebce066eba551c7c76
enels <enomauwaifo@gmail.com>
2023-03-10 17:47:38 +0100

    create the _isalpha func

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+/**
+ * _islapha - checks if its an alphabetic character
+ * @c: character to check
+ *
+ * Return: 1 if alphabetic, else 0
+ */
+
+int _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	
+	return (0);
+}
