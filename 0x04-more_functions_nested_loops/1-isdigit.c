e80da8cc38861c30bf158ba24cfa23dbd5199507
enels <enomauwaifo@gmail.com>
2023-03-12 09:43:21 +0100

    func to check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - checks for a digit
+ * @c: variable to check
+ *
+ * Return: nothing
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= '0' && c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
