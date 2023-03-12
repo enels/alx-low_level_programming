6b3d0c26181d3cc1f643c2f31591c8cbed5cec18
enels <enomauwaifo@gmail.com>
2023-03-12 11:45:57 +0100

    print diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times character '\' should be printed
+ *
+ * Return: nothing
+ */
+
+void print_diagonal(int n)
+{
+	int upper_count; /* number of lines to form the diagonal */
+	int inner_count; /* number of char per line encountered */
+
+	for (upper_count = 0; upper_count < n; upper_count++)
+	{
+		for (inner_count = 0; inner_count <= upper_count; inner_count++)
+		{
+			if (inner_count != upper_count)
+			{
+				_putchar(' '); /* still before the last digit */
+			}
+			else
+			{
+				_putchar('\\'); /* last digit */
+			}
+		}
+		_putchar('\n');
+	}
+}
