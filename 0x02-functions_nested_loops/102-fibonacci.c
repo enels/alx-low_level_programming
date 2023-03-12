3fc3ccbded07b4c6ec24f0e66d723f14c7302370
enels <enomauwaifo@gmail.com>
2023-03-12 07:43:32 +0100

    print the first 50 fibonacci numbers start with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * fibonnaci50 - prints the first 50 fibonacci numbers
+ *
+ * Return: nothing
+ */
+
+#define LIMIT 50
+
+void fibonnaci50(void)
+{
+	long int prev = 1; /* initialize the previous */
+	long int current = 2; /* initialize the current */
+	int old_prev; /* previous before the current previous */
+	int count;
+
+	printf("%li, %li ", prev, current);
+
+	for (count = 0; count < LIMIT - 2; count++)
+	{ 
+		printf(", ");
+
+		old_prev = prev; /* the previous before the previous cum current */
+
+		prev = current; /* stores the current as the previous */
+
+		current = prev + old_prev; /* sum up the previous and the old previous */
+
+		printf("%li", current);
+	}
+
+	printf("\n");
+}
