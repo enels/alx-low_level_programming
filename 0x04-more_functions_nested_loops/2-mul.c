7678d1900af9a66b2c29c3992211722ea9d094e2
enels <enomauwaifo@gmail.com>
2023-03-12 09:52:08 +0100

    create func to multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of the two numbers (an int)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
