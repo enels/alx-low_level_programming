3b3ca894fd19bbda64c72b847efa817cca664087
enels <enomauwaifo@gmail.com>
2023-03-12 07:33:26 +0100

    prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints out 9 times table
+ *
+ * Return: Nothing
+ */
+
+void times_table(void)
+{
+	int times = 9;
+	int times_count;
+	int others;
+	int answer;
+
+	for (times_count = 0; times_count <= times; times_count++)
+	{
+		for (others = 0; others <= times; others++)
+		{
+			answer = others * times_count;
+
+			if (answer >= 10)
+			{
+				_putchar('0' + answer / 10);
+				_putchar('0' + answer % 10);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + answer);
+			}
+			
+			if (others < times)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
