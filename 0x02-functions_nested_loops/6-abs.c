d3b9cf8158919ea182a87895f43926f13a173c2b
enels <enomauwaifo@gmail.com>
2023-03-12 07:30:17 +0100

    create absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/**
+ * _abs - get the absolute value of an integer
+ * @i: value to find its abs
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		;
+	}
+	else
+	{
+		i *= -1;
+	}
+
+	return (i);
+}
